74516cd705537f2f8437347d4d2813845fe90140
Joca <jch8369065@gmail.com>
2025-08-09 18:41:45 -0600

    participant crud service finished

---
+++ HyruleEvents/src/main/java/com/joca/model/participant/ParticipantTypeEnum.java
@@ -0,0 +1,4 @@
+package com.joca.model.participant;
+
+public class ParticipantTypeEnum {
+}
---
+++ HyruleEvents/src/main/java/com/joca/services/ParticipantService.java
@@ -0,0 +1,55 @@
+package com.joca.services.ParticipantService;
+
+import com.joca.database.participant.ParticipantDB;
+import com.joca.model.participant.Participant;
+import com.joca.model.exceptions.DuplicatedKeyException;
+import com.joca.model.exceptions.NotFoundException;
+import com.joca.model.exceptions.NotRowsAffectedException;
+import com.joca.model.filter.Filter;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ParticipantService {
+
+    private ParticipantDB participantDB;
+
+    public ParticipantService(ParticipantDB participantDB) {
+        this.participantDB = participantDB;
+    }
+
+    public void createParticipant(Participant participant) throws SQLException, DuplicatedKeyException, NotRowsAffectedException {
+        if (isKeyInUse(participant.getEmail())) {
+            throw new DuplicatedKeyException("Error al crear el participante, el correo : " + participant.getEmail() + " ya esta en uso");
+        }
+        participantDB.insert(participant);
+    }
+
+    public Participant getParticipantByID(String participantID) throws SQLException, NotFoundException {
+        return participantDB.findByKey(participantID);
+    }
+
+    public List<Participant> getAllParticipants() throws SQLException, NotFoundException {
+        return participantDB.findAll();
+    }
+
+    public void updateParticipant(Participant participant, String originalParticipantID) throws SQLException, DuplicatedKeyException, NotRowsAffectedException {
+        if (isKeyInUse(participant.getEmail())) {
+            throw new DuplicatedKeyException("Error al actualizar el participante, el correo : " + participant.getEmail() + " ya esta en uso");
+        }
+
+        participantDB.updateByKey(participant,originalParticipantID);
+    }
+
+    public void deleteParticipant(String participantID) throws SQLException, NotRowsAffectedException {
+        participantDB.deleteByKey(participantID);
+    }
+
+    public boolean isKeyInUse(String participantID) throws SQLException {
+        return participantDB.isKeyInUse(participantID);
+    }
+
+    public List<Participant> getParticipantsByFilter(List<Filter> filters) throws SQLException, NotFoundException {
+        return participantDB.findByAttributes(filters);
+    }
+}
