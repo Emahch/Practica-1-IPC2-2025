079c09469862951bab056c84c8d0daac52bc62a8
Joca <jch8369065@gmail.com>
2025-08-17 10:37:42 -0600

    iniciando frontend

--- HyruleEvents/src/main/java/com/joca/Main.java
+++ HyruleEvents/src/main/java/com/joca/Main.java
@@ -1,12 +1,12 @@
 package com.joca;
 
-import com.joca.frontend.RegisterSearch;
+import com.joca.frontend.FramePrincipal;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        RegisterSearch mainFrame = new RegisterSearch();
+        FramePrincipal mainFrame = new FramePrincipal();
         mainFrame.setVisible(true);
 
     }
